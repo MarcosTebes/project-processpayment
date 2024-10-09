b6c59db289668eed514859ed37245026f1c6a6d5
marcos tebes <marcosalexistebes@hotmail.com>
2024-10-09 18:20:30 -0300

    nuevo proyecto

---
+++ PaymentGatewayInterface.php
@@ -0,0 +1,10 @@
+<?php
+interface PaymentGateway
+{
+
+    public function processPayment($amount);
+    public function refundPayment($transactionId);
+    public function cancelPayment($transactionId);
+}
+
+?>
